d28e327b6a7baa75135081a67ffaaaff65c0892c
Guilherme <guilhermecostagodoi@gmai.com>
2024-10-20 18:53:34 -0300

    criação do projeto e das classes

---
+++ lib/principal/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _App();
+  }
+
+}
+
+class _App extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
